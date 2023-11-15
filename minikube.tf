b85ea6dc6f062ee1f173a83084ef0627039798de
nishal <nishalk9399@gmail.com>
2023-11-15 21:29:31 +0530

    pushing minikube resources files

--- minikube.tf
+++ minikube.tf
@@ -4,8 +4,7 @@ module "minikube" {
   aws_region    = "us-east-1"
   cluster_name  = "roboshop"
   aws_instance_type = "t3.medium"
-  # ~ --> home dir, you should have terraform.pub in your home
-  ssh_public_key = "C/Users/ADMIN/nishal.pub" # replace with your public key name and path
+  ssh_public_key = "~/nishal.pub" # public key name and path
   aws_subnet_id = "subnet-0f2b61e2aa490ca4f" # any public subnet in any VPC, I am using default VPC subnet
   hosted_zone = "nishaldevops.online" # replace with your domain
   hosted_zone_private = false
